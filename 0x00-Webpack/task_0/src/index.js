e11f0781318ef724a9f6c6bbd34cb5c09b9c82be
PimehT <tareerepimeh@gmail.com>
2024-07-22 19:24:08 +0100

    task 1

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,6 +1,6 @@
 import $ from "jquery";
 
-$(document).ready(function(){
+document.addEventListener("DOMContentLoaded", function(){
   $('body').append('<p>Holberton Dashboard</p>',
     '<p>Dashboard data for the students</p>',
     '<p>Copyright - Holberton School</p>')
