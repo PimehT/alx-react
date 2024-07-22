ed1977c9c2c82961c3c8c433cda4f04349438345
PimehT <tareerepimeh@gmail.com>
2024-07-23 00:20:13 +0100

    task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,18 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import '../css/main.css';
+
+$(function () {
+  $('body').append('<p>Holberton Dashboard</p>',
+    '<p>Dashboard data for the students</p>',
+    '<button id="start-btn">Click here to get started</button>',
+    '<p id="count">{count}</p>',
+    '<p>Copyright - Holberton School</p>'
+  );
+  let count = 0;
+  const updateCounter = _.debounce(() => {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  }, 300);
+  $('#start-btn').on('click', updateCounter);
+});
---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+module.exports = {
+  mode: 'production',
+  entry: './js/dashboard_main.js',
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+              publicPath: 'assets/'
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
