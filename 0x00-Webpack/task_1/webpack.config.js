d99a0008ff06a1f7a61a57f2c9ed6350d25dedd3
PimehT <tareerepimeh@gmail.com>
2024-07-22 21:11:13 +0100

    task 1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'production',
@@ -7,15 +8,9 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
-      },
-    ],
-  },
-}
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+    }),
+  ],
+};
