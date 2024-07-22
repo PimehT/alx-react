cf37c2f7d36bfae9e7f6f28f21840782f0e157a1
PimehT <tareerepimeh@gmail.com>
2024-07-22 20:21:13 +0100

    task 1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,19 +1,21 @@
 import $ from 'jquery';
+import _ from 'lodash';
 
-$(function () {
+function updateCounter() {
+  let count = 0;
+  return function() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  };
+}
+
+$(document).ready(function () {
   $('body').append('<p>Holberton Dashboard</p>',
     '<p>Dashboard data for the students</p>',
     '<button>Click here to get started</button>',
     '<p id="count"></p>',
     '<p>Copyright - Holberton School</p>'
   );
-  updateCounter();
+  const countUpdate = updateCounter();
+  $('button').on('click', _.debounce(countUpdate, 300));
 });
-
-let count = 0;
-function updateCounter () {
-  $('button').on('click', function () {
-    count += 1;
-    $('#count').text(`${count} clicks on the button`);
-  });
-}
