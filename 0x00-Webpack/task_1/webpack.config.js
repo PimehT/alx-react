3dff175bbe0f838ecce32b917dbc2ff5620d3e63
PimehT <tareerepimeh@gmail.com>
2024-07-22 21:20:38 +0100

    task 1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -8,6 +8,17 @@ module.exports = {
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
   },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       filename: 'index.html',
