62d5700789ce2d838dcde7f0885df26d7d643d8e
PimehT <tareerepimeh@gmail.com>
2024-07-29 20:18:30 +0100

    task 2

---
+++ 0x00-Webpack/task_2/js/dashboard_main.js
@@ -0,0 +1,23 @@
+import $ from 'jquery';
+import _ from 'lodash';
+import image from '../assets/holberton-logo.jpg';
+
+function updateCounter() {
+  let count = 0;
+  return function() {
+    count++;
+    $('#count').text(`${count} clicks on the button`);
+  };
+}
+
+$(function() {
+  $('body').append('<div id="logo"></div>')
+  $('body').append('<p>Holberton Dashboard</p>');
+  $('body').append('<p>Dashboard data for the students</p>');
+  $('body').append('<button>Click here to get started</button>');
+  $('body').append('<p id="count"></p>');
+  $('body').append('<p>Copyright - Holberton School</p>');
+
+  const countUpdate = updateCounter();
+  $('button').on('click', _.debounce(countUpdate, 500));
+});
