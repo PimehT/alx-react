c93c4467430a97f9756ab5bdd3325380013ea5fe
PimehT <tareerepimeh@gmail.com>
2024-07-29 19:31:07 +0100

    task 1

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     main: path.resolve(__dirname, './js/dashboard_main.js'),
   },
   output: {
-    path: path.resolve(__dirname, 'public'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
   }
 }
