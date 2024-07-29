fe8a734bdc9a203c6969dd1778096c5f5b9c6559
PimehT <tareerepimeh@gmail.com>
2024-07-29 18:47:31 +0100

    task 0

--- 0x00-Webpack/task_0/src/index.js
+++ 0x00-Webpack/task_0/src/index.js
@@ -1,7 +1,7 @@
-import $ from 'jquery';
+import $ from "jquery";
 
-$(function () {
-  $('body').append('<p>Holberton Dashboard</p>');
-  $('body').append('<p>Dashboard data for the students</p>');
-  $('body').append('<p>Copyright - Holberton School</p>');
-});
+const p1 = $("<p>").text("Holberton Dashboard");
+const p2 = $("<p>").text("Dashboard data for the students");
+const p3 = $("<p>").text("Copyright - Holberton School");
+
+$("body").append(p1, p2, p3);
