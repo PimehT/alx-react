5ddbfd86ddbb93f722ca548af1ae1e8c580b012d
PimehT <tareerepimeh@gmail.com>
2024-07-30 01:54:30 +0100

    task 3

---
+++ 0x02-react_props/task_2/dashboard/src/Login/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Login.css';
+
+
+export default function Login () {
+  return (
+    <>
+      <p>Login to access to the full dashboard</p>
+      <form className='Login-form'>
+        <label htmlFor="email">Email:</label>
+        <input type="email" name="email" id="email" />
+        <label htmlFor="password">Password:</label>
+        <input type="password" name="password" id="password" />
+        <button type="submit">OK</button>
+      </form>
+    </>
+  )
+}
---
+++ 0x02-react_props/task_2/dashboard/src/Notifications/Notifications.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './Notifications.css';
+import Close from '../assets/close-icon.png';
+import NotificationItem from './NotificationItem';
+
+export default function Notifications () {
+  const handleClick = () => {
+    console.log('Close button has been clicked');
+  };
+
+  const getLatestNotification = () => {
+    return '<strong>Urgent requirement</strong> - complete by EOD';
+  }
+
+  return (
+    <div className='Notifications' style={{position: 'relative'}}>
+      <button
+        aria-label='Close'
+        onClick={handleClick}
+        style={
+          {position: 'absolute',
+            right: '.2rem',
+            top: '.2rem',
+            border: 'none',
+            background: 'none'
+          }
+        }
+      >
+        <img src={Close} alt='close-icon' style={{ width: '.5rem'}} />
+      </button>
+      <p>Here is the list of notifications</p>
+      <ul>
+        <NotificationItem type='default' value='New course available' />
+        <NotificationItem type='default' value='New resume available' />
+        <NotificationItem type='urgent' html={getLatestNotification()} />
+      </ul>
+    </div>
+  );
+}
