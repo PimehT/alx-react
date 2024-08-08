8a77d3395975138a7ad4fc5c2396ebbf2322faaa
PimehT <tareerepimeh@gmail.com>
2024-08-08 20:32:26 +0100

    alx-react

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './App.css';
 import Notifications from '../Notifications/Notifications';
@@ -8,33 +8,37 @@ import Header from '../Header/Header';
 import CourseList from '../CourseList/CourseList';
 import { getLatestNotification } from '../utils/utils';
 
-function App({ isLoggedIn }) {
-  const listCourses = [
-    { id: 1, name: 'ES6', credit: 60 },
-    { id: 2, name: 'Webpack', credit: 20 },
-    { id: 3, name: 'React', credit: 40 },
-  ];
+class App extends Component {
+  render() {
+    const { isLoggedIn } = this.props;
 
-  const listNotifications = [
-    { id: 1, type: 'default', value: 'New course available' },
-    { id: 2, type: 'urgent', value: 'New resume available' },
-    { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
-  ];
+    const listCourses = [
+      { id: 1, name: 'ES6', credit: 60 },
+      { id: 2, name: 'Webpack', credit: 20 },
+      { id: 3, name: 'React', credit: 40 },
+    ];
 
-  return (
-    <>
-      <Notifications listNotifications={listNotifications} />
-      <div className='App'>
-        <Header />
-        {isLoggedIn ? (
-          <CourseList listCourses={listCourses} />
-        ) : (
-          <Login />
-        )}
-        <Footer />
-      </div>
-    </>
-  );
+    const listNotifications = [
+      { id: 1, type: 'default', value: 'New course available' },
+      { id: 2, type: 'urgent', value: 'New resume available' },
+      { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+    ];
+
+    return (
+      <>
+        <Notifications listNotifications={listNotifications} />
+        <div className='App'>
+          <Header />
+          {isLoggedIn ? (
+            <CourseList listCourses={listCourses} />
+          ) : (
+            <Login />
+          )}
+          <Footer />
+        </div>
+      </>
+    );
+  }
 }
 
 App.propTypes = {
