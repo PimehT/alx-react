2bce3afe3f0f07a417db47a18dcfefe8400528dc
PimehT <tareerepimeh@gmail.com>
2024-08-09 18:15:00 +0100

    0x04-React_inline_styling

--- 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
+++ 0x04-React_inline_styling/task_0/dashboard/src/CourseList/CourseListRow.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function CourseListRow ({ isHeader, textFirstCell, textSecondCell }) {
+
+function CourseListRow({ isHeader, textFirstCell, textSecondCell }) {
+  const headerRowStyle = { backgroundColor: '#deb5b545' };
+  const rowStyle = { backgroundColor: '#f5f5f5ab', };
+  const selectStyle = isHeader ? headerRowStyle : rowStyle;
+
   return (
-    <tr>
-      {
-        isHeader ? (
-          textSecondCell === null ? (
-            <th colSpan={2}>{textFirstCell}</th>
-          ) : (
-            <>
-              <th>{textFirstCell}</th>
-              <th>{textSecondCell}</th>
-            </>
-          )
+    <tr style={selectStyle}>
+      {isHeader ? (
+        textSecondCell === null ? (
+          <th colSpan={2}>{textFirstCell}</th>
         ) : (
           <>
-            <td>{textFirstCell}</td>
-            <td>{textSecondCell}</td>
+            <th>{textFirstCell}</th>
+            <th>{textSecondCell}</th>
           </>
         )
-      }
+      ) : (
+        <>
+          <td>{textFirstCell}</td>
+          <td>{textSecondCell}</td>
+        </>
+      )}
     </tr>
-  )
+  );
 }
 
 CourseListRow.propTypes = {
   isHeader: PropTypes.bool,
   textFirstCell: PropTypes.string.isRequired,
-  textSecondCell: PropTypes.oneOfType([
-    PropTypes.string,
-    PropTypes.number,
-  ]),
+  textSecondCell: PropTypes.oneOfType([PropTypes.string, PropTypes.number]),
 };
 
 CourseListRow.defaultProps = {
