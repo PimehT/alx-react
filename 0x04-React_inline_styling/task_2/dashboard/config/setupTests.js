44dbdbc9973e9878d9d1242ac24ddabee4a1326c
PimehT <tareerepimeh@gmail.com>
2024-08-12 15:49:18 +0100

    0x04-React_inline_styling

--- 0x04-React_inline_styling/task_2/dashboard/config/setupTests.js
+++
@@ -1,9 +0,0 @@
-import { configure } from 'enzyme';
-import Adapter from '@cfaester/enzyme-adapter-react-18';
-import util from 'util';
-
-configure({ adapter: new Adapter() });
-
-Object.defineProperty(global, 'TextEncoder', {
-  value: util.TextEncoder,
-});
