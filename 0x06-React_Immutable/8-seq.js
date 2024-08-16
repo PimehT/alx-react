897e63b5a88c8d05bd62c0d1eac95c4d46679785
PimehT <tareerepimeh@gmail.com>
2024-08-16 02:32:38 +0100

    0x06-React_Immutable

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -2,12 +2,12 @@ import { Seq } from 'immutable';
 
 export default function printBestStudents(object) {
   const studentsSeq = Seq(object)
-      .filter(student => student.score >= 70)
-      .map(student => ({
-          ...student,
-          firstName: capitalize(student.firstName),
-          lastName: capitalize(student.lastName)
-      }));
+    .filter(student => student.score >= 70)
+    .map(student => ({
+      ...student,
+      firstName: capitalize(student.firstName),
+      lastName: capitalize(student.lastName)
+    }));
   
   function capitalize(word) {
     return word.charAt(0).toUpperCase() + word.slice(1);
