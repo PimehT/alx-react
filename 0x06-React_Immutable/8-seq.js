8608ae0e8f9546179a4509cecd91f5a8aaa0d897
PimehT <tareerepimeh@gmail.com>
2024-08-16 01:57:16 +0100

    0x06-React_Immutable

---
+++ 0x06-React_Immutable/8-seq.js
@@ -0,0 +1,17 @@
+import { Seq } from 'immutable';
+
+function capitalize(word) {
+  return word.charAt(0).toUpperCase() + word.slice(1);
+}
+
+export default function printBestStudents(grades) {
+  const studentsSeq = Seq(grades)
+      .filter(student => student.score >= 70)
+      .map(student => ({
+          ...student,
+          firstName: capitalize(student.firstName),
+          lastName: capitalize(student.lastName)
+      }));
+
+  console.log(studentsSeq.toJS());
+}
