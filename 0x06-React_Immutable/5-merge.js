476e6653e88731dc25b5425aa4e644acfc11a457
PimehT <tareerepimeh@gmail.com>
2024-08-16 01:19:36 +0100

    0x06-React_Immutable

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,15 +1,5 @@
 import { List, Map } from 'immutable';
 
-export const concatElements = (page1, page2) => {
-  const list1 = List(page1);
-  const list2 = List(page2);
+export const concatElements = (page1, page2) => List(page1).concat(List(page2));
 
-  return list1.concat(list2);
-};
-
-export const mergeElements = (page1, page2) => {
-  const map1 = Map(page1);
-  const map2 = Map(page2);
-
-  return map1.merge(map2);
-}
+export const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
